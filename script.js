d3bd0fc8d1e57b6b0f26dcba9329a227bd17aa3d
neviliadita17 <61975618+neviliadita17@users.noreply.github.com>
2023-12-14 00:57:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,120 +1,109 @@
 // -------- Header --------
-// Rotation Logo
-var rotationCount = 0;
+$(document).ready(function () {
+    // Rotation Logo
+    $('#logoImage').click(function () {
+        $(this).addClass('rotate');
+      
+        var rotationCount = $(this).data('rotationCount') || 0;
+        rotationCount++;
+      
+        var rotationAngle = 360 * rotationCount;
+      
+        $(this).css('transform', 'rotate(' + rotationAngle + 'deg)').data('rotationCount', rotationCount);
+      
+        setTimeout(function () {
+          $(this).removeClass('rotate');
+        }.bind(this), 500);
+    });
 
-function rotateLogo() {
-    var logoImage = document.getElementById('logoImage');
+    // Nav toggle
+    $('.burger').click(function () {
+        if (window.innerWidth <= 768) {
+          $('#navLinks').toggle();
+        }
+    });
 
-    logoImage.classList.add('rotate');
-
-    rotationCount++;
-
-    var rotationAngle = 360 * rotationCount;
-
-    logoImage.style.transform = 'rotate(' + rotationAngle + 'deg)';
-
-    setTimeout(function() {
-        logoImage.classList.remove('rotate');
-    }, 500); 
-}
-
-// -------- Navbar --------
-function toggleNav() {
-    var navLinks = document.getElementById("navLinks");
-
-    if (window.innerWidth <= 768) {
-        // Jika layar <= 768px, atur display sesuai dengan keadaan saat ini
-        navLinks.style.display = (navLinks.style.display === "flex" || navLinks.style.display === "") ? "none" : "flex";
-    }
-}
-// Menangani perubahan lebar layar untuk menyembunyikan menu saat lebar layar diperbesar
-window.addEventListener('resize', function () {
-    var navLinks = document.getElementById("navLinks");
-    if (window.innerWidth > 768) {
-        navLinks.style.display = "flex";
-    } else {
-        // Jika layar <= 768px, pastikan menu ditutup
-        navLinks.style.display = "none";
-    }
-});
+    // Resize and Hide menu om display
+    $(window).resize(function () {
+        $('#navLinks').toggle(window.innerWidth > 768);
+    });
 
 // -------- Login --------
-// Validate Form
-function validateUsername() {
-    var username = document.getElementById('username').value;
+    // Username on Login Form
+    $('#username').on('input', function () {
+        var username = $(this).val();
+        var errorElement = $('#usernameError');
+      
+        errorElement.html(username === "" ? "Field Username!" : ($.trim(username) === "" ? "Username cannot be just spaces!" : ""));
+    });
 
-    if (username === "") {
-        document.getElementById('usernameError').innerHTML = "Field Username!";
-    } else if (username.trim() == "") {
-        document.getElementById('usernameError').innerHTML = "Username cannot be just spaces!";
-    } else {
-        document.getElementById('usernameError').innerHTML = "";
-    }
-}
+    // Password on Login Form
+    $('#password').on('input', function () {
+        var password = $(this).val();
+        var hasUppercase = /[A-Z]/.test(password);
+        var hasLowercase = /[a-z]/.test(password);
+        var hasNumber = /\d/.test(password);
+      
+        var passwordError = (!hasUppercase ? " 1 Uppercase," : "") +
+                            (!hasLowercase ? " 1 Lowercase," : "") +
+                            (!hasNumber ? " 1 Number," : "");
+      
+        // Menghapus koma terakhir
+        passwordError = passwordError.slice(0, -1);
+      
+        var errorElement = $('#passwordError');
+      
+        errorElement.html(password === "" ? "Field Password!" : (password.length < 4 || passwordError !== "" ? "Password must be at least 4 characters with:" + passwordError : ""));
+    });
+      
+    // Validate Form
+    $('#form-login').submit(function (event) {
+        event.preventDefault();
+      
+        // Reset pesan kesalahan sebelum validasi
+        $('#usernameError').html("");
+        $('#passwordError').html("");
+      
+        // Validasi username
+        var username = $('#username').val();
+        if (username === "" || $.trim(username) === "") {
+          $('#usernameError').html("Field Username Correctly!");
+        }
+      
+        // Validasi password
+        var password = $('#password').val();
+        var hasUppercase = /[A-Z]/.test(password);
+        var hasLowercase = /[a-z]/.test(password);
+        var hasNumber = /\d/.test(password);
+        var passwordError = (!hasUppercase ? " 1 Uppercase," : "") +
+                            (!hasLowercase ? " 1 Lowercase," : "") +
+                            (!hasNumber ? " 1 Number," : "");
+        passwordError = (password === "" || password.length < 4) ? "Field Password Correctly!" : (passwordError ? "Password must be at least 4 characters with:" + passwordError.slice(0, -1) : "");
+        $('#passwordError').html(passwordError);
+      
+        // Cek apakah ada pesan kesalahan
+        if (!$('#usernameError').html() && !$('#passwordError').html()) {
+            // Jika tidak ada kesalahan, arahkan ke halaman about.html
+            window.location.href = 'about.html';
+        }
+    });
+      
+    // Show Password
+    $('#showPasswordCheckbox').change(function () {
+        var passwordInput = $('#password');
+        passwordInput.prop('type', function (_, currentType) {
+          return currentType === 'password' ? 'text' : 'password';
+        });
+    });
 
-function validatePassword() {
-    var password = document.getElementById('password').value;
-    var hasUppercase = /[A-Z]/.test(password);
-    var hasLowercase = /[a-z]/.test(password);
-    var hasNumber = /\d/.test(password);
-    var hasSpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(password);
-
-    var passwordError = "";
-
-    if (!hasUppercase) passwordError += " 1 Uppercase,";
-    if (!hasLowercase) passwordError += " 1 Lowercase,";
-    if (!hasNumber) passwordError += " 1 Number,";
-    if (!hasSpecialChar) passwordError += " 1 Special Character,";
-
-    // Menghapus koma terakhir
-    passwordError = passwordError.slice(0, -1);
-
-    if (password === "") {
-        document.getElementById('passwordError').innerHTML = "Field Password!";
-    } else if (password.length < 4 || passwordError !== "") {
-        document.getElementById('passwordError').innerHTML = "Password must be at least 4 characters with: " + passwordError;
-    } else {
-        document.getElementById('passwordError').innerHTML = "";
-    }
-}
-
-function validateForm(event) {
-    event.preventDefault();
-    validateUsername();
-    validatePassword();
-
-    // Ambil pesan kesalahan dari kedua validasi
-    var usernameErrorMessage = document.getElementById('usernameError').innerHTML;
-    var passwordErrorMessage = document.getElementById('passwordError').innerHTML;
-
-    // Cek apakah tidak ada pesan kesalahan
-    if (usernameErrorMessage === "" && passwordErrorMessage === "") {
-        // Jika tidak ada kesalahan, arahkan ke halaman about.html
-        window.location.href = 'about.html';
-    }
-}
-
-// Show Password
-function showPassword() {
-    var passwordInput = document.getElementById('password');
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-    } else {
-        passwordInput.type = 'password';
-    }
-}
-
-// CapsLock
-function checkCapsLock(event) {
-    var capsLockWarning = document.getElementById("capsLockWarning");
-    // Mendapatkan status Caps Lock dari event
-    var isCapsLockOn = event.getModifierState && event.getModifierState("CapsLock");
-    // Menampilkan atau menyembunyikan pesan peringatan
-    capsLockWarning.style.display = isCapsLockOn ? "inline" : "none";
-}
-
-// Zoom Profile Picture on aboutme 
-function zoomImage() {
-    var profilePicture = document.querySelector('.profile-picture');
-    profilePicture.classList.toggle('zoomed');
-}
+    // Check CapsLock
+    $('#username, #password').on('keydown keyup', function (event) {
+        $("#capsLockWarning").css("display", event.originalEvent.getModifierState && event.originalEvent.getModifierState("CapsLock") ? "inline" : "none");
+    });
+      
+// -------- About Me --------
+    // Zoom Profile Picture on aboutme 
+    $('.profile-picture').click(function () {
+        $(this).toggleClass('zoomed');
+    });
+});
